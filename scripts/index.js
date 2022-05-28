e3c16a06acfffa3e03f1e49162a59dd16a36fc06
auroraptor <interestingbutno@yandex.ru>
2022-05-28 17:30:56 +0300

    fix: организовать файловую структуру по бэм

--- scripts/index.js
+++ scripts/index.js
@@ -3,30 +3,23 @@
 const page = document.querySelector('.page');
 const profile = page.querySelector('.profile'); // профиль лучше один раз к документу обратиться так-то
 const popup = page.querySelector('.popup'); // секция поп-ап которую надо из секции превратить в div;
-const imposter = page.querySelector('.imposter'); // а эт второй div в котором форма добавления карточк;
-const figure = page.querySelector('.imagine-imposter');
-console.log(figure); // а это третий div в котоором открытие картинки будет
+const imposter = page.querySelector('.imposter'); // а эт второй div в котором форма добавления карточк будет лучше если он станет popup_type_;
+const figure = page.querySelector('.imagine-imposter'); // третий div
 const editButton = profile.querySelector('.profile__edit-button');
-// const closeIcon = popup.querySelector(".popup__close-icon");
 const saveButton = popup.querySelector(".save-button");
 const addButton = profile.querySelector('.add-button'); // вот моя кнопка добавления карточки, которая открывает imposter
-const elements = page.querySelector('.elements');
+const elements = page.querySelector('.elements'); // вот тут тоже не по бэм название но я не понимаю нейминг есть идея назвать эту переменную section
 const card = page.querySelector('#card').content; // получить элемент template достучаться до содержимого, обратившись к свойству content
-
-// const closeIconNewItemForm = page.querySelector('.new-item-form__close-icon'); // а нужна ли она нам?
-
 const formElement = page.querySelector('.form'); // Найти форму в DOM
 const newItemForm = imposter.querySelector('.form_new-item'); // а вот вторая форма imposter
-let closeIcons = page.querySelectorAll('.popup__close-icon'); // все крестики
-
-
-const nameInput = formElement.querySelector('.form__item_input_name');
+const closeIcons = page.querySelectorAll('.popup__close-icon'); // все крестики
+const nameInput = formElement.querySelector('.form__item_input_name');  // поля формы
 const jobInput = formElement.querySelector('.form__item_input_job');
-const placeInput = newItemForm.querySelector('.form__item_input_place');
-const linkInput = newItemForm.querySelector('.form__item_input_link');
-
-const name = profile.querySelector('.name');
-const job = profile.querySelector('.job');
+const newLocation = newItemForm.querySelector('.form__item_input_place'); // поля формы добавления новой карточки
+const newLink = newItemForm.querySelector('.form__item_input_link');
+const newItemButtonSubmit = newItemForm.querySelector('.form__submit-button');
+const name = profile.querySelector('.name'); // поле имени в профиле
+const job = profile.querySelector('.job'); // второе поле в профиле
 
 const initialCards = [
   {
@@ -64,21 +57,21 @@ function renderCard(name, link) {
   photo.alt = name;
   title.textContent = name;
 
-  let like = cardElement.querySelector('.like-button');
+  let like = cardElement.querySelector('.like-button'); // лайк
   like.addEventListener('click', () => {
     like.classList.toggle('like-button_active');
   })
 
-  let move = cardElement.querySelector('.element__delete-button');
+  let move = cardElement.querySelector('.element__delete-button'); // урна
   move.addEventListener('click', () => {
     let item = move.closest('.element');
     item.remove();
   });
 
-  photo.addEventListener('click', () => {
-    figure.classList.toggle('popup_opened'); //+ тут все работает идем дальше
-    let photoIsOpened = figure.querySelector('.popup__image');
-    let photoIsOpenedCaption = figure.querySelector('.popup__caption');
+  photo.addEventListener('click', () => { // картинка
+    figure.classList.toggle('popup_opened'); //+ тут все работает идем дальше и это 7 строка;
+    let photoIsOpened = figure.querySelector('.popup__image'); // 7 строка 3 попап
+    let photoIsOpenedCaption = figure.querySelector('.popup__caption'); // 7 строка 3 попап
     photoIsOpened.src = link;
     photoIsOpened.alt = name;
     photoIsOpenedCaption.textContent = name;
@@ -86,51 +79,33 @@ function renderCard(name, link) {
 
   elements.prepend(cardElement);
 }
-// renderCard('Холмогорская корова моя любимая корова', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'); //+
 
 initialCards.reverse().forEach( item => {
   let name = item.name;
   let link = item.link;
   renderCard(name, link);
 });
-// initialCards.forEach( item => {
-//   let cardElement = card.querySelector('.element').cloneNode(true); // клонировать содержимое тега template
-//   cardElement.querySelector('.element__photo').src = item.link; //наполнить содержимым
-//   cardElement.querySelector('.element__photo').alt = item.name;
-//   cardElement.querySelector('.element__title').textContent = item.name;
 
-//   // повесить лайк и сюда?
-//   let likeButton = cardElement.querySelector('.like-button');
-//   likeButton.addEventListener('click', () => {
-//     likeButton.classList.toggle('like-button_active');
-//   });
-//   // ну и удалим заодно
-//   const deleteButton = cardElement.querySelector('.element__delete-button');
-//   deleteButton.addEventListener('click', () => {
-//     let item = deleteButton.closest('.element'); //метод closest возвращает ближайший родительский элемент с переданным селектором.
-//     item.remove();
-//   });
-
-//   elements.append(cardElement); // отобразить на странице
-// }); // а вот отсюда можно методом forEach пройти по массиву initialCard и создать 6 элемнтов карт на страницу при помощи template
-
-//ФУНКЦИИ
-// функция togglePopup манипулирует css-классом видимости попапа TODO переписать функцию DRY чтобы toggleElem все 3 попапа открывала
-// function togglePopup() {
-//   popup.classList.toggle('popup_opened');
-// }
+// когда идёт клик, в аргументы обработчику влетает браузерное событие - Event. У этого event есть поле target - элемент, по которому кликнули. В нашем случае - это будет кнопка с крестиком. Можно найти ближайший попап - это будет 100% именно сейчас открытый, и убрать класс именно у него.
+// console.log(closeIcons); // В NodeList элементы упорядочены, можно обратиться к свойству length и воспользоваться методом forEach
+closeIcons.forEach( item => {
+  console.log(item);
+  item.addEventListener('click', (evt) => {
+    const eventTarget = evt.target;
+    const grandpa = eventTarget.parentElement.parentElement; //метод closest возвращает ближайший родительский элемент с переданным селектором. попробовать его здесь и так будет даже лучше
+    toggleForm(grandpa);
+  });
+}); // урааааа работает но можно и лучше всегда можно лучше
 
 function toggleForm(element) {
   element.classList.toggle('popup_opened');
-} // функция toggleForm делает все то же что и togglePopup только не привязана к конкретному попапу
+} // функция toggleForm манипулирует css-классом видимости попапа
 
 // функция openedForm заполняет поля «Имя» и «О себе» теми значениями, которые отображаются на странице и лучше ей другое название придумать но какое
 function openedForm() {
   nameInput.value = name.textContent;
   jobInput.value = job.textContent;
-  //  внести вызов функции togglePopup
-  // togglePopup();
-  toggleForm(popup);
+  toggleForm(popup); // 5 строка
 }
 
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
@@ -141,7 +116,17 @@ function formSubmitHandler(evt) {
     name.textContent = nameInputValue; // Вставить новые значения с помощью textContent
     job.textContent = jobInputValue;
 
-    toggleForm(popup); // togglePopup();
+    toggleForm(popup); // 5 строка
+}
+
+// функция обработчика отправки формы для создания новой карточки addNewItemSubmitHander
+function newItemSubmitHandler(evt) {
+  evt.preventDefault();
+
+  let newLocationValue = newLocation.value; // получила значения полей
+  let newLinkValue = newLink.value;
+  renderCard(newLocationValue, newLinkValue);
+  toggleForm(imposter); // 6 строка
 }
 
 // TODO можно реализовать закрытие попапа по клику на любую область вокруг, см Livecooding "Работа с DOM" вторая часть после 80 минут
@@ -154,65 +139,7 @@ formElement.addEventListener('submit', formSubmitHandler);
 
 // начинаю слушать кнопку add-button
 addButton.addEventListener('click', () => {
-  toggleForm(imposter);
-  // imposter.classList.toggle('popup_opened');
+  toggleForm(imposter); // 6 строка
 });
-
-console.log(imposter); //хочу посмотреть что у меня вообще лежит в самозванце
-console.log(newItemForm); // и в новой форме тоже интересн что есть
-// найти поля новой формы и кнопку submit и записать их в переменные
-const newLocation = newItemForm.querySelector('.form__item_input_place');
-const newLink = newItemForm.querySelector('.form__item_input_link');
-const newItemButtonSubmit = newItemForm.querySelector('.form__submit-button');
-console.log(newLocation, newLink, newItemButtonSubmit);// +
-// функция обработчика отправки формы для создания новой карточки
-function newItemSubmitHandler(evt) {
-  evt.preventDefault();
-
-  let newLocationValue = newLocation.value; // получила значения полей
-  let newLinkValue = newLink.value;
-  renderCard(newLocationValue, newLinkValue);
-  // клонирую template
-  // let cardElement = card.querySelector('.element').cloneNode(true);
-  // // наполняю сожержимым
-  // cardElement.querySelector('.element__title').textContent = newLocationValue;
-  // cardElement.querySelector('.element__photo').src = newLinkValue;
-  // cardElement.querySelector('.element__photo').alt = newLocationValue;
-  // // а может здесь повесить обработчик клика?
-  // let likeButton = cardElement.querySelector('.like-button');
-  // likeButton.addEventListener('click', () => {
-  //   likeButton.classList.toggle('like-button_active');
-  // });
-  // // и тут надо удалять еще
-  // const deleteButton = cardElement.querySelector('.element__delete-button');
-  // deleteButton.addEventListener('click', () => {
-  //   let item = deleteButton.closest('.element'); //метод closest возвращает ближайший родительский элемент с переданным селектором.
-  //   item.remove();
-  // }); // все это надо не дублировать а вынести в отдельную функцию renderCard
-
-  // elements.prepend(cardElement);
-
-  toggleForm(imposter);
-
-  // console.log(elements);
-}
-
+// и слушаю кнопку создания нового айтема
 newItemButtonSubmit.addEventListener('click', newItemSubmitHandler);
-
-// Смотри, когда идёт клик, в аргументы обработчику влетает браузерное событие - Event. У этого event есть поле target - элемент, по которому кликнули. В нашем случае - это будет кнопка с крестиком. Можно найти ближайший попап - это будет 100% именно сейчас открытый, и убрать класс именно у него.
-// console.log(closeIcons); // В NodeList элементы упорядочены, можно обратиться к свойству length и воспользоваться методом forEach
-closeIcons.forEach( item => {
-  console.log(item);
-  item.addEventListener('click', (evt) => {
-    const eventTarget = evt.target;
-    const grandpa = eventTarget.parentElement.parentElement; //метод closest возвращает ближайший родительский элемент с переданным селектором. попробовать его здесь
-    toggleForm(grandpa);
-  });
-}); // урааааа работает значит можно удалять лишнее обращения к отдельным крестикам!
-
-
-// открытие попапа с картинкой будет здесь
-
-// const figure = page.querySelector('.imagine-imposter');
-// console.log(figure);
-
