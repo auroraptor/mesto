7dc5faf08e97fb64b2150276e0be8fda3d82d962
auroraptor <interestingbutno@yandex.ru>
2022-06-14 19:39:56 +0300

    feat: validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,2 +1,84 @@
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове функция enableValidation , которая включает валидацию, принимает на вход объект параметров, а затем
+// передаёт параметры вложенным функциям;
+
+// enableValidation({
+//   formSelector: '.popup__form',
+//   inputSelector: '.popup__input',
+//   submitButtonSelector: '.popup__button',
+//   inactiveButtonClass: 'popup__button_disabled',
+//   inputErrorClass: 'popup__input_type_error',
+//   errorClass: 'popup__error_visible'
+// });
+
+// начнём же -- нам нужны три функции isValid проверяет валидность инпута, показывает или скрывает ошибку
+
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.vald) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__error_visible');
+}
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.queryElement(`.${inputElement.id}-error`);
+
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__error_visible');
+  errorElement.textContent = '';
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__button');
+
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach( (inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+}
+
+// обойдем все формы функцией enableValidation
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+
+  formList.forEach( (formElement) => {
+    setEventListeners(formElement);
+  });
+}
+
+enableValidation();
+
+// допишем функции которых не хватает -- управление другими элементами DOM переключиение стилизации кнопки в зависимости от валидности инпутов
+
+// проверим валидность формы и вызовем функцию toggleButtonState
+
+function hasInvalidInput(inputList) {
+  return inputList.some( (inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__button_disabled');
+  } else {
+    buttonElement.classList.remove('popup__button_disabled');
+  }
+}
 
 
