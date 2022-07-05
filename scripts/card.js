ad204b5349c3746911428f26665d4819ad807a27
auroraptor <interestingbutno@yandex.ru>
2022-07-05 11:59:24 +0300

    fix: прокачать гибкость кода, заменить неопределенность определенным отсуствием

--- scripts/card.js
+++ scripts/card.js
@@ -2,10 +2,17 @@ export class Card {
 constructor(data, selector, openPopup) {
   this._name = data.name;
   this._link = data.link;
-  this._template = document.querySelector(selector).content.children[0];
+
+  // разметка может измениться в любой момент, а одно из правил, которого нужно придерживаться при написании кода - это его гибкость
+
+    // this._template = document.querySelector(selector).content.children[0];
+    // гибкость -- это правило, а разметка ненадежно изменчива! Вот этой строки не было среди ошибок, но поняв, что такое гибкость в написании кода, я решила, что и её надо изменить >>> the enter
+
+  this._template = document.querySelector(selector).content.querySelector('.element');
   this._openPopup = openPopup;
   this._popup = document.querySelector('.image-zoomed-popup');
   this._image = this._popup.querySelector('.popup__image');
+  this._caption = this._popup.querySelector('.popup__caption');
 }
 
 _getTemplate() {
@@ -28,9 +35,10 @@ _like() { // <3
 _zoomIn() {
   this._image.src = this._link;
   this._image.alt = this._name;
-  this._image.nextElementSibling.textContent = this._name;
 
-  // я на 31 строке сделала поиск через следующего соседа потому что я внутри тега <figure>, а у него кроме картинки может быть только figurecaption и мне
+  // Найдите второй элемент так же в конструкторе класса и обращайтесь к нему напрямую
+
+  this._caption.textContent = this._name;
 
       // Чтобы прокинуть функцию открытия в конструктор класса, вам нужно ее здесь принять так:
       // constructor(data, selector, openPopup)
@@ -49,7 +57,13 @@ _remove() {
   // Лучше всего после удаления карточки очистить ссылку на DOM-элемент: this._element = null;
     // -- а мне больше нравится не прародитель всех сущностей, а undefined, я оставлю его, разницы ж не будет (надеюсь хех), в какой из двух примитивов скидывать значение этого приватного поля удаляемого элемента >>> the enter
 
-  this._element = undefined;
+  // this._element = undefined;
+
+  // null является определённым значением отсутствия объекта, тогда как undefined обозначает неопределённость.
+    // теперь я это запомню! Как сказал мой друг: ревью -- эт очень круто, потому что на ревью скажут такие вещи, которые не вычитаешь ^^ >>> the enter
+
+  this._element = null;
+
 }
 
 _setListeners() {
