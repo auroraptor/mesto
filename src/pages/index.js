40422c9a3bfed33105cb619ed46bef0775939a7f
auroraptor <interestingbutno@yandex.ru>
2022-07-18 13:09:17 +0300

    fix: сделать отдельную функцию createCard для создания новой карточки

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,11 +23,24 @@ const handleCardClick = (name, link) => {
   popupWithImage.open(name, link);
 }
 
+const createCard = (item) => {
+  const newCard = new Card(item, '#card', handleCardClick);
+
+  return newCard.generateCard();
+}
+
+// Можно же вставлять карточку в начало с prepend, в конец с append, вообще не вставлять, а сделать массив готовых карточек, а потом уже вставить всё разом в DOM.
+  // я это пойму, но не сразу >>> the enter
+  // ааааа "можно" значит можно реализовать какие-то методы в слое Section которые и будут делать append вставку или создавать массив готовых карточек! >>> the enter
+// И такой пункт есть в чек-листе. Скрин из него http://joxi.ru/xAeobK0cb8D1Gm -- 404 -- но наверн речь про это >> Функция выполняет только одну задачу, например, возвращает разметку карточки. -- каждый раз, когда я сама в одиночку прохожу сквозь чек-лист, мне кажется, будто все правильно и вот как надо, но, очевидно, моё стремление отправить работу на ревью мешает здесь, и я забываю про то что необходимо Проверять себя. Во всем. Постоянно. Помнить про то, что проверка занимает гораздо меньше времени, чем сама работа, зато избавляет от чувсва досады, которое всегда приходит следом за невнимательностью. >>> the enter
+
 const cardList = new Section({
   items: initialCards.reverse(),
   renderer: (item) => { // отвечает за создание и отрисовку данных на странице
-    const newCard = new Card(item, '#card', handleCardClick);
-    const element = newCard.generateCard();
+
+    // ххх ну вот даже что у меня ^ тут ^ стоит СОЗДАНИЕ&ОТРИСОВКА должно было б подвести меня к мысли что это значит 2 действия, а после вспомнить о том что круто, когда у функции одна ответственность одного действия -- я ещё научусь читать и понимать что пишу в своих же комментариях >>> the enter
+
+    const element = createCard(item);
 
     cardList.addItem(element);
     }
@@ -48,8 +61,7 @@ const popupEditProfile = new PopupWithForm('.profile-popup', {
 const popupAddNewItem = new PopupWithForm(
   '.new-item-popup', {
   handleFormSubmit: (formData) => {
-    const card = new Card(formData, '#card', handleCardClick);
-    const element = card.generateCard();
+    const element = createCard(formData);
     cardList.addItem(element);
     }
   }
